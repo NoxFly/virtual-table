b507d95983b7bc9cf7e58fe8905e03c038b6d4e7
NoxFly <dorian.thivolle@gmail.com>
2025-08-13 09:36:12 +0200

    refactor: remove export from type and interface declarations in VirtualTable for cleaner code

--- dist/VirtualTable.d.ts
+++ dist/VirtualTable.d.ts
@@ -1,6 +1,6 @@
-export declare type Any = string | number | boolean | null | undefined | object;
+declare type Any = string | number | boolean | null | undefined | Date | object;
 
-export declare interface Cell<T extends Type> {
+declare interface Cell<T extends Type> {
     $: HTMLElement;
     value: Any;
     row: TreeNode<T>;
@@ -9,7 +9,7 @@ export declare interface Cell<T extends Type> {
     columnIndex: number;
 }
 
-export declare interface ColumnDef<T extends Type> {
+declare interface ColumnDef<T extends Type> {
     title: string;
     field?: keyof T;
     width: number;
@@ -17,102 +17,113 @@ export declare interface ColumnDef<T extends Type> {
     transform?: (cell: Cell<T>) => string;
 }
 
-export declare interface FlatNode<T extends Type> {
-    node: TreeNode<T>;
-    index: number;
-}
-
-export declare interface Position {
-    top: number;
-    left: number;
-}
-
-export declare interface TableRow<T extends Type> {
+declare interface TableRow<T extends Type> {
     $: HTMLElement;
-    ref?: FlatNode<T>;
+    ref?: TreeNode<T>;
     previousElement?: TableRow<T>;
     nextElement?: TableRow<T>;
     x: number;
     y: number;
 }
 
-export declare interface TreeNode<T extends Type> {
+declare interface TreeNode<T extends Type> {
     data: T;
     expanded: boolean;
     depth: number;
     children: TreeNode<T>[];
+    parent?: TreeNode<T>;
+    left?: TreeNode<T>;
+    right?: TreeNode<T>;
+    flatIndex: number;
 }
 
-export declare type Type = {
+declare type Type = {
     id: string | number;
     children?: Type[];
     [key: string]: Any;
 };
 
+declare type UpdatedRow<T extends Type> = Partial<Omit<T, 'children'>> & Pick<T, 'id'>;
+
 export declare class VirtualTable<T extends Type> {
     private readonly container;
     protected static readonly DEFAULT_OPTIONS: VirtualTableOptions;
-    private readonly table;
-    private readonly tableHead;
-    private readonly tableBody;
     private readonly columns;
-    private rows;
-    private data;
+    private readonly rows;
     private tree;
-    private flatten;
+    private readonly flatten;
+    private readonly nodeMap;
     private readonly ROW_HEIGHT;
     private VISIBLE_ROWS_COUNT;
     private TOTAL_VISIBLE_ROWS;
-    private tbodyStartY;
+    private TBODY_START_Y;
+    private readonly $table;
+    private readonly $tableHead;
+    private readonly $tableBody;
     private readonly selectedNodes;
     private readonly selectedCells;
     private readonly selectedColumns;
     private mostTopRow;
+    private $lastHighlightedRow;
     readonly options: VirtualTableOptions;
     constructor(container: HTMLElement, columnsDef: ColumnDef<T>[], options?: Partial<VirtualTableOptions>);
     private get scrollTop();
     private get totalVirtualHeight();
     private get columnUnits();
-    private createColumns;
-    private computeViewbox;
-    private computeInViewVisibleRows;
-    private resetSelections;
-    private getNodeFromRow;
-    private updateViewBoxHeight;
-    private updateRowsContent;
+    private DOM_createColumns;
+    private DOM_computeViewbox;
+    private DOM_computeInViewVisibleRows;
+    private DOM_resetSelections;
+    private DOM_getRowFromHTMLRow;
+    private DOM_getRowIndex;
+    private DOM_updateViewBoxHeight;
+    private DOM_updateRowsContent;
+    private DOM_updateRowContent;
+    private DOM_getTableRowFromNode;
     private formatCellValue;
-    private createEmptyRow;
-    private createEmptyCells;
-    private removeRow;
-    private setRowPosition;
-    private updateScroll;
-    private onScroll;
-    private onClick;
-    private onRowClick;
-    selectRow(event: MouseEvent, row: TableRow<T>): void;
-    selectAllRows(): void;
-    unselectAllRows(): void;
-    selectCell(): void;
-    unselectAllCells(): void;
-    selectColumn(column: ColumnDef<T>): void;
-    unselectAllColumns(): void;
-    editCell(row: TableRow<T>, $cell: HTMLElement): void;
-    cancelCellEdition(): void;
+    private DOM_resetTableRows;
+    private DOM_createEmptyRow;
+    private DOM_createEmptyCells;
+    private DOM_removeRow;
+    private DOM_setRowPosition;
+    private DOM_updateScroll;
+    private lastScrollTopIndex;
+    private DOM_EVENT_onScroll;
+    private DOM_EVENT_onClick;
+    private DOM_EVENT_onRowClick;
     private toggleRowExpand;
-    private resetTableRows;
-    private dataToTreeNode;
-    scrollTo(index: number): void;
+    private dataToTreeNodeRec;
+    private computeTree;
+    private recomputeDataTree;
+    deleteNode(nodeId: string): typeof VirtualTable;
+    deleteNodes(nodeIds: string[]): typeof VirtualTable;
+    addNode(relativeTo: string, asChildren: boolean, element: T): typeof VirtualTable;
+    addNodes(relativeTo: string, asChildren: boolean, elements: T[]): typeof VirtualTable;
+    updateNode(node: UpdatedRow<T>): typeof VirtualTable;
+    updateNodes(nodes: UpdatedRow<T>[]): typeof VirtualTable;
+    private verifyDuplicateIds;
     setData(data: T[]): void;
-    allowColumnResizing(allow: boolean): void;
-    allowRowSelection(allow: boolean): void;
-    allowCellSelection(allow: boolean): void;
-    allowCellEditing(allow: boolean): void;
-    private lastHighlightedRow;
-    makeDroppable(): void;
+    clear(): void;
+    getNodes(): readonly TreeNode<T>[];
+    scrollTo(index: number): typeof VirtualTable;
+    selectRow(event: MouseEvent, row: TableRow<T>): typeof VirtualTable;
+    selectAllRows(): typeof VirtualTable;
+    unselectAllRows(): typeof VirtualTable;
+    selectCell(): typeof VirtualTable;
+    unselectAllCells(): typeof VirtualTable;
+    selectColumn(column: ColumnDef<T>): typeof VirtualTable;
+    unselectAllColumns(): typeof VirtualTable;
+    editCell(row: TableRow<T>, $cell: HTMLElement): typeof VirtualTable;
+    cancelCellEdition(): typeof VirtualTable;
+    allowColumnResizing(allow: boolean): typeof VirtualTable;
+    allowRowSelection(allow: boolean): typeof VirtualTable;
+    allowCellSelection(allow: boolean): typeof VirtualTable;
+    allowCellEditing(allow: boolean): typeof VirtualTable;
+    makeDroppable(): typeof VirtualTable;
     onDrop: (data: string | undefined, row: TableRow<T>) => void;
 }
 
-export declare interface VirtualTableOptions {
+declare interface VirtualTableOptions {
     id: string;
     rowHeight: number;
     defaultExpanded: boolean;
