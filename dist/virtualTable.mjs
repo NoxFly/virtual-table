a7e67d08b6a8e522780354d4bff23730d2dd2656
NoxFly <dorian.thivolle@gmail.com>
2025-08-13 14:33:41 +0200

    chore: update build process and add tsup configuration
    - Changed the build script to run linting before building with tsup.
    - Added a prepush script to ensure the build is executed before pushing.
    - Introduced a new tsup configuration file for building the project with specified options.

--- dist/virtualTable.mjs
+++ dist/virtualTable.mjs
@@ -3,33 +3,8 @@
  * @license MIT
  * @author NoxFly
  */
-"use strict";
 var __defProp = Object.defineProperty;
-var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-var __getOwnPropNames = Object.getOwnPropertyNames;
-var __hasOwnProp = Object.prototype.hasOwnProperty;
 var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
-var __export = (target, all) => {
-  for (var name in all)
-    __defProp(target, name, { get: all[name], enumerable: true });
-};
-var __copyProps = (to, from, except, desc) => {
-  if (from && typeof from === "object" || typeof from === "function") {
-    for (let key of __getOwnPropNames(from))
-      if (!__hasOwnProp.call(to, key) && key !== except)
-        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
-  }
-  return to;
-};
-var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
-
-// src/index.ts
-var src_exports = {};
-__export(src_exports, {
-  EventManager: () => EventManager,
-  VirtualTable: () => VirtualTable
-});
-module.exports = __toCommonJS(src_exports);
 
 // src/EventManager.ts
 var _EventManager = class _EventManager {
@@ -1053,11 +1028,10 @@ _VirtualTable.DEFAULT_OPTIONS = {
   allowRowReorder: false
 };
 var VirtualTable = _VirtualTable;
-// Annotate the CommonJS export names for ESM import in node:
-0 && (module.exports = {
+export {
   EventManager,
   VirtualTable
-});
+};
 /**
  * @copyright Copyright (c) 2025 NoxFly
  * @license AGPL-3.0
@@ -1065,4 +1039,4 @@ var VirtualTable = _VirtualTable;
  * Entry point for the virtualization module.
  * Exports the main VirtualTable component and related type definitions.
  */
-//# sourceMappingURL=virtualTable.js.map
+//# sourceMappingURL=virtualTable.mjs.map
---
+++ tsup.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "tsup";
+
+const copyrights = `
+/**
+ * @copyright 2025 NoxFly
+ * @license MIT
+ * @author NoxFly
+ */
+`.trim()
+
+export default defineConfig({
+    entry: {
+        virtualTable: "src/index.ts"
+    },
+    keepNames: true,
+    minifyIdentifiers: false,
+    name: "virtualTable",
+    format: ["cjs", "esm"],
+    dts: true,
+    sourcemap: true,
+    clean: true,
+    outDir: "dist",
+    target: "es2020",
+    minify: false,
+    splitting: false,
+    shims: false,
+    treeshake: false,
+    banner: {
+        js: copyrights,
+    }
+});
